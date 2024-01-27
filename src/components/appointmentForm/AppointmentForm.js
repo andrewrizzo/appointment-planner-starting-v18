60f929e546d60f1e44e36933b3334ec09687592b
Andrew Rizzo <rizzoa256@gmail.com>
2024-01-27 12:55:46 -0500

    add style apponitment

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -19,41 +19,137 @@ export const AppointmentForm = ({
   setTime,
   handleSubmit
 }) => {
-const [  v, setV] =useState();
- 
-return (
+  const [v, setV] = useState();
+
+  return (
+
     <>
-   <form>
-    <input 
-    type="text"
-    name=" "
-    />
-    <input // name of Person
-    type="text" name="name of person "
-    
-    />
-    <input // date
-    type="date" id="date"
-    
-    />
-    <input // time
-    type="time" id="date"
-    
-    />
-    <input // Address
-    type="address" id="address"
-    
-    />
-    <input // host aka Who are you meeting with?
-    type="text" id="host"
-    
-    
-    />
-    
-    
-    Address
-    
-    </form> 
+      <form
+      className="form"
+        style={{
+          display: "flex",
+          flexDirection: 'column',
+          backgroundColor: "blue",
+          height: 900,
+          width: 400,
+        }}
+      >
+        <div
+        style={{
+        display: "flex",
+        justifyContent: 'flex-end',
+          height: 90,
+          backgroundColor: 'red',
+      
+      }}
+        
+        >
+          <label>
+            what is your your name:
+            <input // name of Person
+              style={{
+                height: 100,
+              marginLeft: 40,
+             width: 145,
+            }}
+              
+              type="text"
+              name="name of person "
+            />
+          </label>
+        </div>
+        <div
+        style={{
+          display: "flex", 
+          height: 90,
+          justifyContent: 'flex-end',
+          backgroundColor: "green",
+        }}
+        
+        >
+          <label>
+            
+            What date is the meeting:
+          </label>
+          <input // date
+             style={{
+              height: 100,
+            marginLeft: 40,
+            width: 145,
+          }}
+            type="date"
+            id="date"
+          />
+        </div>
+        <div
+        style={{
+         display: "flex",
+          height: 90,
+         justifyContent: "flex-end",
+         backgroundColor: "yellow",
+      }}
+        >
+          <label>
+            What time is the meeting:
+            <input // time
+               style={{
+                height: 100,
+              marginLeft: 40,
+              width: 145,
+            }}
+              type="time" id="date"
+            />
+          </label>
+        </div>
+        <div
+        style={{
+         display: "flex",
+          height: 90,
+         justifyContent: "flex-end",
+         backgroundColor: " lightblue",
+        }}
+        
+        >
+          <label>
+            Where are you meeting:
+            <input // Address
+               style={{
+                height: 100,
+              marginLeft: 40,
+              width: 145
+              ,
+            }}
+              type="address"
+              id="address"
+              placeholder="addess"
+            />
+          </label>
+        </div>
+        <div
+        style={{
+         display: "flex",
+          height:  90,
+         justifyContent: "flex-end",
+         backgroundColor: "brown",
+        }}
+        >
+          <label>
+            you lending the meeting:
+            <input // host aka Who are you meeting with?
+              style={{
+                height: 100,
+              marginLeft: 40,
+             width: 145,
+            }} 
+              type="text"
+              id="host"
+            />
+          </label>
+        </div>
+
+      </form>
     </>
+
+
   );
 };
